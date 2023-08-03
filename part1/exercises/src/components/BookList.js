8454fd1df120a27dba7bb8b79138d7e3e3a85090
bahbond <bondopicks@gmail.com>
2023-08-03 15:55:56 -0500

    books

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Reading. It Works";
+   let book1 = "https://i5.walmartimages.com/asr/ad2188a9-0f80-4751-a4a6-6178081ba6ab.77555df837b5cefa93ef69148b17bec7.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF";
+   let book2 = "https://th.bing.com/th/id/OIP.UsWXbawgx_dcK4XnchQe0AHaLF?w=204&h=305&c=7&r=0&o=5&dpr=1.1&pid=1.7";
+   let book3 = "https://th.bing.com/th/id/OIP.kM3KOYIzmcQsqiyYyg9fyAAAAA?pid=ImgDet&w=360&h=541&rs=1";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="A Prayer for Owen Meany" />
+         <img src={book2} alt="The Sun Also Rises" />
+         <img src={book3} alt="Lonesome Dove" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,25 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+
+import React from "react";
+import classes from './ChoresList.module.css';
+
+// export default function ChoresList () {
+//    return <p class="choresText">No content is here! What BOUT</p>;
+// }
+class choresList extends React.Component {
+   render() {
+      const chores = ["Take out the trash", "Do prepwork", "Laundry"];
+      return (
+      <div>
+         <h3 className = {classes.choresHeading}>Today's Chores</h3>
+         <ol>
+            <li className={classes.choresText}>{chores[0]}</li>
+            <li className={classes.choresText}>{chores[1]}</li>
+            <li className={classes.choresText}>{chores[2]}</li>
+
+         </ol>
+      </div>
+      );
+   }
 }
+
+export default choresList
